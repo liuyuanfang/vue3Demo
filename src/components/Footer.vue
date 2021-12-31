f23836d29dfdda21b4917832137f0801651987d7
刘元芳 <18339982700@163.com>
2021-12-31 09:20:18 +0800

    Project Init.

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+   <el-footer style="border: 1px solid blue">Footer</el-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  setup() {
+    
+  },
+  data() {
+    return {
+      menuData: [],
+    };
+  }
+};
+</script>
+
---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-row class="tac">
+    <el-col>
+      <el-menu
+        router="true"
+        @open="handleOpen"
+        @close="handleClose"
+        v-for="(item, index) in menuData"
+        :key="item.menuName"
+      >
+        <el-sub-menu :index="index">
+          <template #title>
+            <el-icon><apple /></el-icon>
+            <span> {{ item.menuName }} </span>
+          </template>
+
+          <el-menu-item
+            v-for="subItem in item.subMenu"
+            :key="subItem.subMenuName"
+            :index="subItem.path"
+          >
+            <el-icon><cherry /></el-icon>
+            <span>{{ subItem.subMenuName }}</span>
+          </el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  setup() {
+    const handleOpen = (key, keyPath) => {
+      console.log(key, keyPath);
+    };
+    const handleClose = (key, keyPath) => {
+      console.log(key, keyPath);
+    };
+    return {
+      handleOpen,
+      handleClose,
+    };
+  },
+  data() {
+    return {
+      menuData: [],
+    };
+  },
+  mounted() {
+    this.$axios
+      .get("/menu.json")
+      .then((response) => {
+        this.menuData = response.data;
+        console.log(this.menuData);
+      })
+      .catch(() => {});
+  },
+};
+</script>
+
